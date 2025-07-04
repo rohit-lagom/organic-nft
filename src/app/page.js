a4138654c58e2311b4c6e4044e660a48911a1adb
rohit-lagom <rohit@lagomchain.com>
2025-07-05 00:37:38 +0530

    fix import

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
-import Code from "../components/code/code";
-import Explore from "../components/explore/explore";
-import { Navbar } from "../components/navbar/navbar";
-import { Hero } from "../components/hero/hero";
-import { Footer } from "../components/footer/footer";
+import Code from "@/components/code/code";
+import Explore from "@/components/explore/explore";
+import { Navbar } from "@/components/navbar/navbar";
+import { Hero } from "@/components/hero/hero";
+import { Footer } from "@/components/footer/footer";
+
 
 import Image from "next/image";
 
